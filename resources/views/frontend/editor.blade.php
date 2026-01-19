2278819555b05da683a6eb2b503d4f736ea70054
NiazSVD <niaz@softvencedelta.com>
2026-01-19 14:13:24 +0600

    Update home title

--- resources/views/frontend/editor.blade.php
+++ resources/views/frontend/editor.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $settings->site_name ?? 'SVD Delta' }} - {{ $language->display_name }}</title>
+    <title>{{ $settings->site_name ?? 'SVD Delta' }} | {{ $language->display_name }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset($settings->favicon ?? '') }}">
 
     <!-- Bootstrap 5 CSS -->
--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Code - @yield('title')</title>
+    <title>{{ $settings->site_name }} | @yield('title')</title>
     <link rel="icon" type="image/x-icon" href="{{ asset($settings->favicon ?? '') }}">
 
     @include('frontend.layouts.styles')
